52f92db8b5f74da62ff5464fb4e07c7ababc1f93
kartikeya vats <kartikeyavats04@gmail.com>
2022-12-28 23:08:56 +0530

    third day completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/home_page.dart';
+import 'package:flutter_application_1/pages/home_page.dart';
+import 'package:flutter_application_1/pages/login_page.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
@@ -10,13 +11,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    
-    
     // ignore: prefer_const_constructors
     return MaterialApp(
-      home:HomePage(),
-      );
-    
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.lightBlue),
+      darkTheme: ThemeData(
+        // brightness: Brightness.dark,
+        primarySwatch: Colors.cyan,
+      ),
+      initialRoute: '/home',
+      routes: {'/': (context) => LoginPage(), '/home': (context) => HomePage()},
+    );
   }
 }
