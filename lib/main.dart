e273a2bbdb49dc0fb53ae15674fbc7b6cee8c186
kartikeya vats <kartikeyavats04@gmail.com>
2022-12-31 16:05:43 +0530

    third day completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/home_page.dart';
 import 'package:flutter_application_1/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,8 +20,11 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData(
         // brightness: Brightness.dark,
         primarySwatch: Colors.cyan,
+        fontFamily: GoogleFonts.lato().fontFamily,
+        
+        // primaryTextTheme: GoogleFonts.latoTextTheme(),
       ),
-      initialRoute: '/home',
+      // initialRoute: '/home',
       routes: {'/': (context) => LoginPage(), '/home': (context) => HomePage()},
     );
   }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -6,12 +7,59 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-          child: Text(
-        "Login Page",
-        style: TextStyle(
-            fontSize: 40, fontWeight: FontWeight.bold, color: Colors.blue),
-      )),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/images/login.png",
+            fit: BoxFit.cover,
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Text(
+            "Welcome ",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter username",
+                    labelText: "Username",
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Enter Password",
+                    labelText: "Password",
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          ElevatedButton(
+            onPressed: (() {
+              print("hi kartikeya this side");
+            }),
+            child: Text("Login"),
+            style: TextButton.styleFrom(),
+          )
+        ],
+      ),
     );
   }
 }
